7563a87e9828bee31a2da4cc9cf7eeb6a147c7a2
= <=>
2022-04-24 11:20:03 -0400

    Ejercicio 4 listo

--- 6. Matrices/Matrices4.cpp
+++ 6. Matrices/Matrices4.cpp
@@ -8,36 +8,32 @@ llenarla de números aleatorios, copiar el contenido a otra matriz y por último
 using namespace std;
 
 int main (){
-    int x, y, dato;
-    //srand(time(NULL));//Generamos el numero aleatorio
+    int x, y;
+    srand(time(NULL));//Generamos el numero aleatorio
 
     cout<<"Ingrese la cantidad de filas que tendrá la matriz: "; cin>>x;
     cout<<"Ingrese la cantidad de columnas que tendrá la matriz:  "; cin>>y;
 
     int matriz[x][y], copia[x][y]; //Declaramos la matriz y su copia
 
-    //Generamos el numero aleatorio
-
     //Rellenamos la matriz con numeros del 1 al 100
     for(int i=0; i<x; i++){
         for(int j=0; j<y; j++){
-            srand(time(NULL));
-            dato=1+rand()%(100);
-            matriz[x][y]=dato; //Lo establecemos en un rango de 1 a 100 en nuestro elemento
+            matriz[i][j] = 1+rand()%(100-1); //Lo establecemos en un rango de 1 a 100 en nuestro elemento
         }
     }
 
     //Copiamos e imprimimos la nueva matriz
-    cout<<"La copia de la matriz generada con numeros del 1 al 100 es: \n";
+    cout<<"\nLa copia de la matriz generada con numeros del 1 al 100 es: \n";
     for(int i=0; i<x; i++){
         for(int j=0; j<y; j++){
-            copia[x][y]=matriz[x][y]; //Copia
+            copia[i][j]=matriz[i][j]; //Copia
 
-            cout<<copia[x][y]<<" "; //Imprime
+            cout<<copia[i][j]<<" "; //Imprime
         }
         cout<<endl;
     }
 
-    system("Pause");
+    system("pause");
     return 0;
 }

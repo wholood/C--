bc8930e28260dd296c7107f46685cad83fed6d00
= <=>
2022-04-25 09:47:15 -0400

    Actualizacion

--- 6. Matrices/Matrices5.cpp
+++ 6. Matrices/Matrices5.cpp
@@ -9,24 +9,24 @@ using namespace std;
 int main (){
     int matriz[3][3];
 
-    for(int i; i<3; i++){
-        for(int j; j<3; j++){
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
             cout<<"["<<i<<"]["<<j<<"] Ingrese un digito: ";
             cin>>matriz[i][j];
         }
     }
 
     cout<<"La matriz es: \n";
-    for(int i; i<3; i++){
-        for(int j; j<3; j++){
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
             cout<<matriz[i][j]<<" ";
         }
         cout<<endl;
     }
 
     cout<<"Y su transpuesta es: \n";
-    for(int i; i<3; i++){
-        for(int j; j<3; j++){
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
             cout<<matriz[j][i]<<" ";
         }
         cout<<endl;
--- 6. Matrices/Matrices6.cpp
+++ 6. Matrices/Matrices6.cpp
@@ -8,38 +8,38 @@ using namespace std;
 int main (){
     int a[3][3], b[3][3], c[3][3];
 
-    cout<<"Ingrese los datos de la matriz a: ";
-    for(int i; i<3; i++){
-        for(int j; j<3; j++){
+    cout<<"Ingrese los datos de la matriz a: \n";
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
             cout<<"a ["<<i<<"]["<<j<<"] Ingrese un digito: ";
             cin>>a[i][j];
         }
     }
 
-    cout<<"Ingrese los datos de la matriz b: ";
-    for(int i; i<3; i++){
-        for(int j; j<3; j++){
+    cout<<"Ingrese los datos de la matriz b: \n";
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
             cout<<"b ["<<i<<"]["<<j<<"] Ingrese un digito: ";
             cin>>b[i][j];
         }
     }
 
-    cout<<"Las matrices son:\n a --\t-- b";
-    for(int i; i<3; i++){
-        for(int j; j<3; j++){
+    cout<<"Las matrices son:\n a --\t-- b\n";
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
             cout<<a[i][j]<<" ";
         }
         cout<<"\t";
-        for(int j; j<3;j++){
+        for(int j=0; j<3;j++){
             cout<<b[i][j]<<" ";
         }
         cout<<endl;
     }
 
 
-    cout<<"El resultado de la suma de las matrices a y b es: ";
-    for(int i; i<3; i++){
-        for(int j; j<3; j++){
+    cout<<"\nEl resultado de la suma de las matrices a y b es: \n";
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
             c [i][j]= a[i][j] + b[i][j];
             cout<<c[i][j]<<" ";
         }
